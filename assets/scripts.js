250909374a0943650e5a3cd490036280d21f5ddd
GenericGithUser <blgold567@gmail.com>
2024-08-24 16:51:34 +0800

    modified:   assets/scripts.js 	modified:   index.html 	modified:   styles/style.css

--- assets/scripts.js
+++ assets/scripts.js
@@ -114,6 +114,18 @@ window.addEventListener("DOMContentLoaded", () => {
     })
 })
 
+//Glide
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) =>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+const hideElements = document.querySelectorAll('.hide');
+hideElements.forEach((element)=> observer.observe(element));
 
 
 
